e5722b1426f9eb7b6e99bc8046339c1b38096d40
crangelsmith <camila.rangel.smith@gmail.com>
2016-03-23 16:46:11 +0100

    Adding mT histos

--- Camila_Project/Regresion.py
+++ Camila_Project/Regresion.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+
+from sklearn import linear_model
 
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import KFold
@@ -8,7 +12,10 @@ from sklearn.cross_validation import KFold
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.cross_validation import cross_val_score
 
+from sklearn import gaussian_process
+
 import matplotlib.pyplot as plt
+import math
 
 pd.set_option('display.max_columns', 500)
 titanic = pd.read_csv("mg5pythia8_hp200.root.test2.csv")
@@ -33,17 +40,17 @@ selector.fit(selected_sample[predictors], selected_sample["pt(mc nuH)"])
 scores = -np.log10(selector.pvalues_)
 
 # Plot the scores.
-plt.bar(range(len(predictors)), scores)
-plt.xticks(range(len(predictors)), predictors, rotation='vertical')
-plt.show()
+#plt.bar(range(len(predictors)), scores)
+#plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+#plt.show()
 
 #picked the best features according to SelectKBest
-predictors_final = ["et(met)", " phi(met)","pt(reco tau1)", "eta(reco tau1)", "phi(reco tau1)","pt(reco bjet1)","pt(reco bjet1)","eta(reco bjet1)","phi(reco bjet1)","m(reco bjet1)","phi(reco bjet3)","m(reco bjet3)","pt(reco bjet4)","eta(reco bjet4)","phi(reco bjet4)","m(reco bjet4)","pt(reco jet1)","eta(reco jet1)","phi(reco jet1)","m(reco jet1)","pt(reco jet2)","eta(reco jet2)","phi(reco jet2)","m(reco jet2)"]
+predictors_final = ["et(met)", " phi(met)","pt(reco tau1)", "eta(reco tau1)", "phi(reco tau1)","pt(reco bjet1)","pt(reco bjet1)","eta(reco bjet1)","phi(reco bjet1)","m(reco bjet1)","phi(reco bjet3)","m(reco bjet3)","pt(reco bjet4)","eta(reco bjet4)","phi(reco bjet4)","m(reco bjet4)","pt(reco jet1)","eta(reco jet1)","phi(reco jet1)","m(reco jet1)","pt(reco jet2)","eta(reco jet2)","phi(reco jet2)","m(reco jet2)","nbjet"]
 
 
 # Initialize our algorithm class
 alg = LinearRegression()
-# Generate cross validation folds for the  dataset.  It return the row indices corresponding to train and test.
+#alg = linear_model.BayesianRidge()# Generate cross validation folds for the  dataset.  It return the row indices corresponding to train and test.
 # We set random_state to ensure we get the same splits every time we run this.
 kf = KFold(selected_sample.shape[0], n_folds=3, random_state=1)
 
@@ -63,6 +70,9 @@ predictions = np.concatenate(predictions, axis=0)
 target=list(selected_sample["pt(mc nuH)"])
 pred=list(predictions)
 met=list(selected_sample["et(met)"])
+met_phi=list(selected_sample[" phi(met)"])
+taupt=list(selected_sample["pt(reco tau1)"])
+tauphi=list(selected_sample["phi(reco tau1)"])
 
 
 ## I want to check the resolution of the prediction wrt the truth value resol = (pt_pred - pt_true)/pt_true*100. I also want to compare it with the resolution from the MET (default).
@@ -70,35 +80,72 @@ met=list(selected_sample["et(met)"])
 result_predict=[]
 result_default=[]
 
+mT_predict=[]
+mT_default=[]
+
+count_fail =0
 for i in range(len(target)):
     porcentage_predict= (target[i] - pred[i])/target[i]*100
     porcentage_met= (target[i] - met[i])/target[i]*100
+
     result_predict.append(porcentage_predict)
     result_default.append(porcentage_met)
 
-result=list(result_predict)
-default=list(result_default)
+    if taupt[i]>0 and pred[i]>0:
+        mt_new=math.sqrt(2*pred[i]*taupt[i]*(1-math.cos(tauphi[i]-met_phi[i])))
+        mt_old=math.sqrt(2*met[i]*taupt[i]*(1-math.cos(tauphi[i]-met_phi[i])))
+
+        mT_predict.append(mt_new)
+        mT_default.append(mt_old)
+    if pred[i]<0:
+        count_fail=count_fail+1
+                                        
+print("% times prediction is <0")
+print(count_fail)
 
 prediction_list = np.array(list(result_predict))
 defaul_list = np.array(list(result_default))
+                                        
+mt_prediction_list = np.array(list(mT_predict))
+mt_defaul_list = np.array(list(mT_default))
+                                        
 
+print("pT resolution")
 print(stats.describe(prediction_list))
 print(stats.describe(defaul_list))
 
-
-plt.hist(result, bins=2000)
+bins=2000
+plt.hist(prediction_list, bins)
 plt.title("Preddicted result")
 plt.xlabel("resolution (%)")
 plt.ylabel("Frequency")
 plt.axis([-200,200,0,600])
 plt.show()
 
-plt.hist(default, bins=2000)
+plt.hist(defaul_list, bins=2000)
 plt.title("default result")
 plt.xlabel("resolution (%)")
 plt.ylabel("Frequency")
 plt.axis([-200,200,0,600])
 plt.show()
+                                        
+print("mT distribution")
+print(stats.describe(mt_prediction_list))
+print(stats.describe(mt_defaul_list))
 
+bins=100
+plt.hist(mt_prediction_list, bins)
+plt.title("Preddicted result")
+plt.xlabel("mT [GeV]")
+plt.ylabel("Frequency")
+plt.axis([0,400,0,400])
+plt.show()
+
+plt.hist(mt_defaul_list, bins)
+plt.title("default result")
+plt.xlabel("resolution (%)")
+plt.ylabel("Frequency")
+plt.axis([0,400,0,400])
+plt.show()
 
 
