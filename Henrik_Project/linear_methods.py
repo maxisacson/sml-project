10ee1a875c9a81dc85f911c200440dbf7f720539
Henrik Ohman <speeph@gmail.com>
2016-04-06 11:23:39 +0200

    Added Henrik_Project
    Added project with three different regression methods on the nuH pT:
    * linear_methods.py: covering linear and ridge regression
    * support_vectors.py: covering support vector regression (SVR) with different
      kernels
    * ensemble_methods.py: covering single decision trees as well as ensamble
      methods such as extra trees, gradient boosting, and random forest

---
+++ Henrik_Project/linear_methods.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+
+from six import iteritems
+
+from scipy import stats
+
+#from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
+
+import matplotlib.pyplot as plt
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = 'Regression using linear methods')
+parser.add_argument('-r',
+                    '--regressors',
+                    nargs = '+',
+                    help='the regressors to use',
+                    metavar='<regressors>')
+parser.add_argument('-a',
+                    '--alpha',
+                    default=10.0,
+                    type=float,
+                    help='the gamma parameter for the RBF kernel',
+                    metavar='<gamma>')
+arguments = parser.parse_args()
+
+# All available variables in the dataset
+all_predictors = [
+    'et(met)', 'phi(met)', 'ntau', 'nbjet', 'njet', 'pt(reco tau1)',
+    'eta(reco tau1)', 'phi(reco tau1)', 'm(reco tau1)', 'pt(reco bjet1)',
+    'eta(reco bjet1)', 'phi(reco bjet1)', 'm(reco bjet1)', 'pt(reco bjet2)',
+    'eta(reco bjet2)', 'phi(reco bjet2)', 'm(reco bjet2)', 'pt(reco bjet3)',
+    'eta(reco bjet3)', 'phi(reco bjet3)', 'm(reco bjet3)', 'pt(reco bjet4)',
+    'eta(reco bjet4)', 'phi(reco bjet4)', 'm(reco bjet4)', 'pt(reco jet1)',
+    'eta(reco jet1)', 'phi(reco jet1)', 'm(reco jet1)', 'pt(reco jet2)',
+    'eta(reco jet2)', 'phi(reco jet2)', 'm(reco jet2)', 'pt(reco jet3)',
+    'eta(reco jet3)', 'phi(reco jet3)', 'm(reco jet3)', 'pt(reco jet4)',
+    'eta(reco jet4)', 'phi(reco jet4)', 'm(reco jet4)'
+]
+# Predictors as selected by SelektKBest
+selected_predictors = [
+    'et(met)', 'phi(met)',
+    'pt(reco tau1)', 'eta(reco tau1)', 'phi(reco tau1)',
+    'pt(reco bjet1)', 'eta(reco bjet1)', 'phi(reco bjet1)', 'm(reco bjet1)',
+    #'phi(reco bjet3)', 'm(reco bjet3)', 'pt(reco bjet4)',
+    #'eta(reco bjet4)', 'phi(reco bjet4)', 'm(reco bjet4)',
+    'pt(reco jet1)', 'eta(reco jet1)', 'phi(reco jet1)', 'm(reco jet1)',
+    #'pt(reco jet2)', 'eta(reco jet2)', 'phi(reco jet2)', 'm(reco jet2)',
+    'nbjet'
+]
+selected_displays = [
+    'et(met)',
+    'pt(mc nuH)',
+    'pt(reco tau1)',
+    'pt(mc tau)',
+    'mass_truth'
+]
+
+target_parameter = 'pt(mc nuH)'
+reco_parameter = 'et(met)'
+pred_parameter = 'pt(pred nuH)'
+reco_resolution = 'et(res met)'
+pred_resolution = 'pt(res pred nuH)'
+
+pd.set_option('display.max_columns', 500)
+sample_200 = pd.read_csv('../test/mg5pythia8_hp200.root.test3.csv')
+#sample_300 = pd.read_csv('../test/mg5pythia8_hp300.root.test.csv')
+
+
+# Make a combined sample
+#dataset = sample_200 + sample_300
+dataset = sample_200
+
+# Replace invalid values with NaN
+dataset = dataset.where(dataset > -998.0, other=0)
+
+# Compute the H+ truth mass
+dataset['mass_truth'] = (
+    np.sqrt(  2
+            * (dataset['pt(mc nuH)'])
+            * (dataset['pt(mc tau)'])
+            * (  np.cosh(dataset['eta(mc nuH)'] - dataset['eta(mc tau)'])
+               - np.cos(dataset['phi(mc nuH)'] - dataset['phi(mc tau)'])))
+)
+
+print(dataset[selected_displays].head(10))
+#print(dataset.head(10)['mass_truth'])
+#print(dataset.describe())
+
+train, global_test = train_test_split(dataset,
+                                      test_size = 0.3,
+                                      random_state = 0)
+train_predictors = train[selected_predictors]
+targets = train[target_parameter]
+
+sc = StandardScaler()
+sc.fit(train_predictors)
+train_predictors_std = sc.transform(train_predictors)
+
+available_regressors = {
+    'linear': (
+        'Linear Regression',
+        LinearRegression(),
+    ),
+    'ridge': (
+        'Ridge Regression',
+        Ridge(alpha=arguments.alpha),
+    ),
+    # Yeah, LogisticRegression is a classifier, not a regressor...
+    #'logistic': (
+        #'Logistic Regression',
+        #LogisticRegression(C=arguments.cost,
+                           #max_iter=arguments.iterations),
+    #),
+}
+
+regressors = [
+    (k, v)
+    for k, v
+    in iteritems(available_regressors)
+    if k in arguments.regressors
+]
+
+for key,(name,regressor) in regressors:
+    print('Regression with {}'.format(name))
+
+    regressor.fit(train_predictors_std, targets)
+
+    test = global_test.copy()
+    test_predictors = test[selected_predictors]
+    test_predictors_std = sc.transform(test_predictors)
+
+    test.loc[:,pred_parameter] = regressor.predict(test_predictors_std)
+    test.loc[:,reco_resolution] = (test[target_parameter] - test[reco_parameter]) \
+        / test[target_parameter] * 100.0
+    test.loc[:,pred_resolution] = (test[target_parameter] - test[pred_parameter]) \
+        / test[target_parameter] * 100.0
+
+    print(test[[target_parameter, pred_parameter, reco_parameter]].head(10))
+
+    colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00']
+
+    params = { 'figure.facecolor': 'white',
+               'figure.subplot.bottom': 0.0,
+               'font.size': 16, 
+               'legend.fontsize': 16,
+               'legend.borderpad': 0.2,
+               'legend.labelspacing': 0.2,
+               'legend.handlelength': 1.5,
+               'legend.handletextpad': 0.4,
+               'legend.borderaxespad': 0.2,
+               'lines.markeredgewidth': 2.0,
+               'lines.linewidth': 2.0,
+               'axes.prop_cycle': plt.cycler('color',colors)}
+    plt.rcParams.update(params)
+
+    fig, ax = plt.subplots(2)
+    ax[0].hist(test[pred_parameter], 50, range=(0.0, 400.0), label='Pred', histtype='step')
+    ax[0].hist(test[reco_parameter], 50, range=(0.0, 400.0), label='Obs', histtype='step')
+    ax[0].hist(test[target_parameter], 50, range=(0.0, 400.0), label='Truth', histtype='step')
+    ax[0].set_xlim([0,400])
+    ax[0].set_xlabel("pT (GeV)")
+    ax[0].legend(loc='upper right')
+
+    ax[1].hist(test[pred_resolution], 50, range=(-100.0, 100.0), label='Pred', histtype='step')
+    ax[1].hist(test[reco_resolution], 50, range=(-100.0, 100.0), label='Obs', histtype='step')
+    ax[1].set_xlim([-100,100])
+    ax[1].set_xlabel("Resolution (%)")
+    ax[1].legend(loc='upper left')
+
+    fig.tight_layout(pad=0.3)
+    fig.savefig('{}_a{}.pdf'.format(key, arguments.alpha))
+    plt.show()
